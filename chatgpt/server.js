a4a1c9b77c7e886d2c811d0cef0d8b4a60a5f14d
srivarshabachu <srivarshabachu@gmail.com>
2023-12-02 22:37:38 +0530

    commit

--- chatgpt/server.js
+++ chatgpt/server.js
@@ -1,10 +1,11 @@
 const PORT = 8000
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 const app = express()
 app.use(express.json())
 app.use(cors())
-const API_KEY = ''
+const API_KEY = process.env.API_KEY
 app.post('/completions', async (req, res) => {
     const options = {
         method: "POST",
