1fc2a84deb4fd59c220d71f1a66b59529faa11c0
srivarshabachu <srivarshabachu@gmail.com>
2023-12-02 12:36:19 +0530

    commit

--- chatgpt/src/App.js
+++ chatgpt/src/App.js
@@ -5,12 +5,24 @@ const App = () => {
       <section className="side-bar">
         <button>+ New Chat</button>
         <ul className="history">
+          <li>BLUGH</li>
         </ul>
         <nav>
-          <p>made by </p>
+          <p>made by varsha</p>
         </nav>
       </section>
       <section className="main">
+        <h1>ChatGPT</h1>
+        <ul className="feed"></ul>
+        <div className="bottum-section">
+          <div className="input-container">
+            <input />
+            <div id="submit">➤</div>
+          </div>
+          <p className="info">
+            ChatGPT can make mistakes. Consider checking important information.
+          </p>
+        </div>
       </section>
     </div>
   );
